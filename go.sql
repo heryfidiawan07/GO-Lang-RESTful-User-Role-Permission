298b36d99be7b365d828952aa2896861161524e7
Hery Fidiawan <heryfidiawan07@gmail.com>
2022-02-10 03:48:14 +0700

    Add sql file

---
+++ go.sql
@@ -0,0 +1,20 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : MySQL Local
+ Source Server Type    : MySQL
+ Source Server Version : 100411
+ Source Host           : localhost:3306
+ Source Schema         : sk_restfull_api
+
+ Target Server Type    : MySQL
+ Target Server Version : 100411
+ File Encoding         : 65001
+
+ Date: 10/02/2022 03:46:45
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+SET FOREIGN_KEY_CHECKS = 1;
